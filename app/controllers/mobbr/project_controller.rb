c6ba3e753ba7023b4198e7667814a3283f72b2bb
Cesar A. Sulbaran P <cesulbaran@gmail.com>
2015-01-23 19:03:57 -0430

    add files

--- app/controllers/mobbr/project_controller.rb
+++ app/controllers/mobbr/project_controller.rb
@@ -1,8 +1,19 @@
 class Mobbr::ProjectController < ApplicationController
   
+  def initialize 
+   @data  = { participants: []}  
+  end
   
   def show 
-    
+    Project.where(archived: false) do |project|
+      project.milestone.participants.each do |participant|
+        @data[:participants].push({
+            :id => participant.email,
+            :role => "coder",
+            :share => "3"
+        })
+      end
+    end
   end
   
   def issues    
