161a58b30d699e2e0fb31102c5f69f44f61092f8
Cesar A. Sulbaran P <cesulbaran@gmail.com>
2015-01-24 03:04:29 -0430

    add participant for issues

--- app/controllers/mobbr/project_controller.rb
+++ app/controllers/mobbr/project_controller.rb
@@ -7,7 +7,7 @@ class Mobbr::ProjectController < ApplicationController
   end
   
   def show 
-  if !params[:group].nil? && !params[:project].nil? && !params[:id].nil?
+  if !params[:group].nil? && !params[:project].nil?
    project = _projects.find_by(path: params[:project]) 
    if project
      @group.group_members.each do  |member|
@@ -24,8 +24,28 @@ class Mobbr::ProjectController < ApplicationController
     render :json => @data 
   end
   
-  def issues    
-    render :json => @data 
+  def issues 
+    if !params[:group].nil? && !params[:project].nil? && !params[:id].nil?
+      @issue = _issue.find_by!(iid: params[:id])
+      if @issue
+         @issue.participants.each do |member|
+           rol = nil
+           @group.group_members.each do  |memb|
+            if member.email == memb.user.email
+              Gitlab::Access.options.keys.collect{|x| rol =  x if Gitlab::Access.options[x] == memb.access_level}
+              Gitlab::Access.options.keys.collect{|x| rol =  '' if  memb.access_level.blank?}
+            end
+           end
+          
+          @data[:participants].push({
+              :id => member.email,
+              :role =>  rol ,
+              :share => '3'
+           })           
+         end
+        render :json => @data
+      end
+    end
   end
   
   def merge_requests
@@ -36,6 +56,13 @@ class Mobbr::ProjectController < ApplicationController
     
   end
   
+  
+  def _issue
+    @proj =    _projects.find_by(path: params[:project])
+    @issue ||= @proj.issues
+
+  end
+  
   def _projects
     @project ||=  _groups.projects
   end
@@ -43,6 +70,6 @@ class Mobbr::ProjectController < ApplicationController
   def _groups
     @group   ||= Group.find_by(path: params[:group])
   end
-  
+
   
 end
